41e07aba487fef79a9284a4ed2dc517ff7241065
Enma Leticia Ramírez Castro <eramirezc38@miumg.edu.gt>
2024-02-16 16:19:32 -0600

    program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            resultadosMatematicos objOp = new resultadosMatematicos();
+            Console.WriteLine("12.8 * 8.4 = " + objOp.multiplicacion(14.8, 22.44));
+
+            Console.Write("Introduce algun numero: ");
+            objOp.ParametroUno = Double.Parse(Console.ReadLine());
+            Console.Write("Escribe un numero");
+            objOp.ParametroDos = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Su resultado es: " + objOp.multiplicacion());
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+
+    
+}
+
+
---
+++ resultadosMatematicos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    public class resultadosMatematicos
+    {
+        private double resultado;
+        private double parametroUno, parametroDos;
+
+        public double Resultado { get => resultado; set => resultado = value; }
+        public double ParametroUno { get => parametroUno; set => parametroUno = value; }
+        public double ParametroDos { get => parametroDos; set => parametroDos = value; }
+
+        public  double multiplicacion(double valorUno, double valorDos)
+        {
+            resultado = valorUno * valorDos;
+
+            return resultado;
+
+        }
+
+        public double multiplicacion()
+        {
+            this.Resultado = this.ParametroUno * ParametroDos;
+
+            return this.Resultado;
+        }
+       
+      
+            
+            
+       
+
+
+
+           
+    }
+}
